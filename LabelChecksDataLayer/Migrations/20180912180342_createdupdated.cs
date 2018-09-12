6968b66b2fce0a2852ecaa2871adae89b74b211b
nagasudhirpulla <nagasudhirpulla@gmail.com>
2018-09-12 23:58:21 +0530

    Fixes #3

---
+++ LabelChecksDataLayer/Migrations/20180912180342_createdupdated.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LabelChecksDataLayer.Migrations
+{
+    public partial class createdupdated : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateCreated",
+                table: "LabelCheckResults",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateModified",
+                table: "LabelCheckResults",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateCreated",
+                table: "LabelCheckResults");
+
+            migrationBuilder.DropColumn(
+                name: "DateModified",
+                table: "LabelCheckResults");
+        }
+    }
+}
