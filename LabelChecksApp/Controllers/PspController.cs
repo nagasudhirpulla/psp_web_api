2bd109631421e47dc0d9c233cf8c5e7170df966a
nagasudhirpulla <nagasudhirpulla@gmail.com>
2018-12-29 16:38:52 +0530

    done

--- LabelChecksApp/Controllers/PspController.cs
+++ LabelChecksApp/Controllers/PspController.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
 using PSPDataFetchLayer.DbUtils;
 using PSPDataFetchLayer.Models;
 
@@ -50,5 +51,32 @@ namespace LabelChecksApp.Controllers
         public void Post([FromBody] string value)
         {
         }
+
+        // GET: api/psp/labels
+        [HttpGet("labels")]
+        public async Task<ActionResult<List<PspLabelApiItem>>> GetData()
+        {
+            PspDbHelper helper = new PspDbHelper { ConnStr = Configuration["pspdbinfo:ConnectionString"] };
+            List<PspLabelApiItem> pspDbMeasurements = new List<PspLabelApiItem>();
+            try
+            {
+                List<PspMeasurement> measurements = await _context.PspDbMeasurements.ToListAsync();
+                for (int measIter = 0; measIter < measurements.Count; measIter++)
+                {
+                    pspDbMeasurements.Add(new PspLabelApiItem { Label = measurements[measIter].Label, Id = measurements[measIter].MeasId });
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return pspDbMeasurements;
+        }
+    }
+
+    public class PspLabelApiItem
+    {
+        public string Label { get; set; }
+        public int Id { get; set; }
     }
 }
